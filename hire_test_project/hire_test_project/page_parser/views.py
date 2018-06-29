6b477c693f0883b4dbd87110f8e81301f43413a1
Sergey Ondrin <sondrin@bellintegrator.ru>
2018-06-29 15:12:25 +0300

    suggestions fulfilled

--- hire_test_project/hire_test_project/page_parser/views.py
+++ hire_test_project/hire_test_project/page_parser/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.views import APIView
 
 from hire_test_project.tasks import parse_html_page
@@ -10,10 +11,12 @@ class Tags(APIView):
 
     def post(self, request):
         data = TagsPostInputSerializer(data=request.data)
-        data.is_valid(raise_exception=True)
+        if not data.is_valid():
+            return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
         task = parse_html_page.delay(data.validated_data['url'])
         outp = TagsPostOutputSerializer(data={'task_id': task.id})
-        outp.is_valid(raise_exception=True)
+        if not outp.is_valid():
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response(outp.data)
 
     def get(self, request):
@@ -25,12 +28,14 @@ class Tags(APIView):
         result = {'code': 1}
 
         data = TagsGetInputSerializer(data=request.query_params)
-        data.is_valid(raise_exception=True)
+        if not data.is_valid():
+            return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
         task_id = data.validated_data['task_id']
         parsed_page = parse_html_page.AsyncResult(task_id).result
         if parsed_page and not isinstance(parsed_page, Exception):
             result['code'] = 0
             result.update(parsed_page)
         outp = TagsGetOutputSerializer(data=result)
-        outp.is_valid(raise_exception=True)
+        if not outp.is_valid():
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response(outp.data)
--- hire_test_project/hire_test_project/tasks.py
+++ hire_test_project/hire_test_project/tasks.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup as soup
 
 @shared_task(bind=True, default_retry_delay=5)
 def parse_html_page(self, url):
-    # r = requests.get(url, timeout=(5,30))
     try:
         r = requests.get(url, timeout=(1,30))
     except requests.exceptions.ConnectionError as exc:
@@ -12,7 +11,19 @@ def parse_html_page(self, url):
     s = soup(r.text, 'html.parser')
     res = {}
     for tag in ('a', 'h1', 'h2', 'h3'):
-        res[tag] = len(s.find_all(tag))
-    anchors = s.find_all('a', {'href': True})
-    res['urls'] = [a['href'] for a in anchors if a['href']]
+        tags = s.find_all(tag)
+        if tag == 'a':
+            res['urls'] = [a['href'] for a in tags if a.get('href', None)]
+        #     hrefs = []
+        #     c = 0
+        #     for a in tags:
+        #         c += 1
+        #         href = a.get('href', None)
+        #         if href:
+        #             hrefs.append(href)
+        #     res[tag] = c
+        #     res['urls'] = hrefs
+        # else:
+        #     res[tag] = len(tags)
+        res[tag] = len(tags)
     return res
